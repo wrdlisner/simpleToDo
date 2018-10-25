e292e9861383a043e19bf9b89db04301fa5f2a3c
袖川航平 <kouhei.sodekawa@gmail.com>
2018-10-25 20:46:11 +0900

    1st commit Done:topPage In Progress:tableCell

--- simpleToDo/ViewController.swift
+++ simpleToDo/ViewController.swift
@@ -1,25 +1,24 @@
-//
-//  ViewController.swift
-//  simpleToDo
-//
-//  Created by 袖川航平 on 2018/10/24.
-//  Copyright © 2018 袖川航平. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var todoList: UITableView!
+    @IBOutlet weak var todoListCell: UITableViewCell!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 
 }
-
